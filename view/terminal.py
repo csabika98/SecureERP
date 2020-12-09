5b10a10e52cdf635e71d1563df4cc54c8cb20299
csabika98 <6ix9inegotti@naver.com>
2020-12-09 10:45:23 +0100

    atleastrunning

--- view/terminal.py
+++ view/terminal.py
@@ -1,3 +1,24 @@
+# colors
+RESET = "\033[0;0m"
+BOLD = "\033[;1m"
+REVERSE = "\033[;7m"
+RED = '\033[31m'
+GREEN = '\033[32m'
+ORANGE = '\033[33m'
+BLUE = '\033[34m'
+PURPLE = '\033[35m'
+CYAN = '\033[36m'
+LIGHTGRAY = '\033[37m'
+DARKGRAY = '\033[90m'
+LIGHTRED = '\033[91m'
+LIGHTGREEN = '\033[92m'
+YELLOW = '\033[93m'
+LIGHTBLUE = '\033[94m'
+PINK = '\033[95m'
+LIGHTCYAN = '\033[96m'
+
+
+
 def print_menu(title, list_options):
     """Prints options in standard menu format like this:
 
@@ -11,7 +32,12 @@ def print_menu(title, list_options):
         title (str): the title of the menu (first row)
         list_options (list): list of the menu options (listed starting from 1, 0th element goes to the end)
     """
-    pass
+    order_number = 0
+    print(BOLD + '\n'*2 + title + RESET)
+    for option in list_options:
+        print(BOLD + "     (" + str(order_number) + ") ", option + RESET)
+        order_number += 1
+   
 
 
 def print_message(message):
@@ -20,7 +46,7 @@ def print_message(message):
     Args:
         message: str - the message
     """
-    pass
+    print(BLUE + "MESSAGE: " + message + RESET)
 
 
 def print_general_results(result, label):
@@ -39,13 +65,47 @@ def print_general_results(result, label):
 # |--------|------------|----------|
 # |   1    | Sidewinder | missile  |
 # \-----------------------------------/
-def print_table(table):
+def print_table(table, title_list):
     """Prints tabular data like above.
 
     Args:
         table: list of lists - the table to print out
     """
-    pass
+    row_width = []
+    for width in title_list:
+        row_width.append(len(width))
+    for line in range(0, len(table)):
+        for index in range(0, len(table[line])):
+            if len(table[line][index]) > row_width[index]:
+                row_width[index] = len(table[line][index])
+
+    table_lenght = 0
+    for index in range(0, len(row_width)):
+        row_width[index] += 2
+        table_lenght += row_width[index]
+
+    print(GREEN + "/" + "-" * (table_lenght+len(title_list)-1) + "\\" + RESET)
+
+    for i in range(0, len(title_list)):
+        temp = round((row_width[i] - len(title_list[i]))/2)
+        print(GREEN + "|" + " " * temp + BOLD + title_list[i], end='')
+        print(GREEN + " " * (row_width[i] - (temp + len(title_list[i]))), end='')
+    print("|" + RESET)
+
+    for line in range(0, len(table)):
+        for width in row_width:
+            print(GREEN + "|" + "-" * width, end='')
+        print("|" + RESET)
+        for index in range(0, len(table[line])):
+            temp = round((row_width[index] - len(table[line][index]))/2)
+            print(GREEN + "|" + " " * temp + BLUE + table[line][index], end='')
+            print(GREEN + " " * (row_width[index]-temp-len(table[line][index])), end='')
+        print("|" + RESET)
+
+    print(GREEN + "\\" + "-" * (table_lenght+len(title_list)-1) + "/" + RESET)
+
+    input(REVERSE + "Press enter to continue" + RESET)
+    
 
 
 def get_input(label):
@@ -54,16 +114,23 @@ def get_input(label):
     Args:
         label: str - the label before the user prompt
     """
-    pass
+    inputs = get_inputs([CYAN + "Please enter a number: " + RESET], "")
+    return inputs[0]
 
 
-def get_inputs(labels):
+
+def get_inputs(labels, title):
     """Gets a list of string inputs from the user.
 
     Args:
         labels: list - the list of the labels to be displayed before each prompt
     """
-    pass
+    inputs = []
+    print(CYAN + title + RESET)
+    for label in labels:
+        user_input = input(label)
+        inputs.append(user_input)
+    return inputs
 
 
 def print_error_message(message):
@@ -72,4 +139,4 @@ def print_error_message(message):
     Args:
         message: str - the error message
     """
-    pass
+    print(RED + "ERROR:" + message + RESET)
